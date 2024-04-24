78eab571317e7a05dea6c5d5faa457ef9298535d
PartyWumpus <48649272+PartyWumpus@users.noreply.github.com>
2024-04-24 08:55:59 +0100

    Switch to Option<Cell>

--- src/main.rs
+++ src/main.rs
@@ -13,25 +13,22 @@ enum Color {
 
 #[derive(Debug, Clone)]
 struct Cell {
-    color: Color,
-    empty: bool,
+    color: Color, 
 }
 
-impl std::fmt::Display for Cell {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let symbol = match self {
-            Cell {empty:true, ..} => " ".normal(),
-            Cell {empty:false, color:Color::Blue} => "B".blue(),
-            Cell {empty:false, color:Color::Red} => "R".red(),
-            Cell {empty:false, color:Color::Green} => "G".green(),
-        };
-        write!(f, "{}", symbol)
-    }
+fn fmt_cell(cell: &Option<Cell>) -> String {
+    let symbol = match cell {
+        None => " ".normal(),
+        Some(Cell {color:Color::Blue}) => "B".blue(),
+        Some(Cell {color:Color::Red}) => "R".red(),
+        Some(Cell {color:Color::Green}) => "G".green(),
+    };
+    format!("{} ", symbol)
 }
 
 #[derive(Debug)]
 struct CellGrid {
-    grid: Array2D<Cell>,
+    grid: Array2D<Option<Cell>>,
     score: usize,
 }
 
@@ -44,18 +41,14 @@ struct Match {
 }
 
 impl Cell {
-    fn new_random() -> Self {
-        Self {
+    fn new_random() -> Option<Self> {
+        Some(Self {
             color: rand::random(),
-            empty: false,
-        }
+        })
     }
 
-    fn new_empty() -> Self {
-        Self {
-            color: Color::Blue,
-            empty: true,
-        }
+    fn new_empty() -> Option<Self> {
+        None
     }
 }
 
@@ -76,7 +69,6 @@ impl CellGrid {
     }
 
     fn delete_matches(self: &mut Self) -> usize {
-        //let mut matches = vec![];
         let mut score = 0;
         let col_matches = self.grid.columns_iter().enumerate().flat_map(check_line).collect::<Vec<_>>();
         let row_matches = self.grid.rows_iter().enumerate().flat_map(check_line).collect::<Vec<_>>();
@@ -85,7 +77,7 @@ impl CellGrid {
             println!("col {col_match:?}");
             for x in col_match.inner_index-col_match.length..col_match.inner_index {
                 if let Some(element) = self.grid.get_mut(x, col_match.outer_index) {
-                    element.empty = true;
+                    *element = None;
                 }
             }
             score += col_match.length;
@@ -95,7 +87,7 @@ impl CellGrid {
             println!("row {row_match:?}");
             for y in row_match.inner_index-row_match.length..row_match.inner_index {
                 if let Some(element) = self.grid.get_mut(row_match.outer_index, y) {
-                    element.empty = true;
+                    *element = None;
                 }
             }
             score += row_match.length;
@@ -106,7 +98,7 @@ impl CellGrid {
 
     fn do_gravity(&mut self) {
         for col_index in 0..self.grid.row_len() {
-            let elements = self.grid.column_iter(col_index).unwrap().filter(|element| element.empty == false).map(|element| element.clone()).collect::<Vec<_>>();
+            let elements = self.grid.column_iter(col_index).unwrap().filter(|element| element.is_some()).map(|element| element.clone()).collect::<Vec<_>>();
             let num_empty = self.grid.column_len() - elements.len();
             for (row_index, element) in elements.into_iter().rev().enumerate() {
                 self.grid.set(self.grid.column_len() - row_index - 1, col_index, element);
@@ -124,7 +116,7 @@ impl CellGrid {
         println!("score: {}", self.score);
         self.grid.rows_iter().for_each(|it| {
             let mut str: String = String::new();
-            it.for_each(|x| str.push_str(&format!("{x} ")));
+            it.for_each(|x| str.push_str(&fmt_cell(x)));
             println!("{str}");
         })
     }
@@ -132,31 +124,32 @@ impl CellGrid {
 
 fn check_line<'a, I>((outer_index, line): (usize, I)) -> Vec<Match>
 where
-    I: Iterator<Item = &'a Cell>,
+    I: Iterator<Item = &'a Option<Cell>>,
 {
     let mut prev: Option<&Cell> = None;
     let mut count = 1;
     let mut length = 0;
     let mut matches = vec![];
 
-    for (inner_index, element) in line.enumerate() {
+    for (inner_index, maybe_element) in line.enumerate() {
         length += 1;
+        let element = maybe_element.as_ref().unwrap();
         if let Some(prev_element) = prev {
-            if prev_element.color == element.color {
+            if prev_element.color == element.color { // TODO no unwrap here
                 count += 1;
             } else {
                 if count >= 3 {
                     matches.push(Match {
                         color: prev_element.color.clone(),
                         length: count,
-                        inner_index: inner_index, 
+                        inner_index, 
                         outer_index,
                     });
                 }
                 count = 1;
             }
         }
-        prev = Some(element)
+        prev = Some(element);
     }
 
     if count >= 3 {
